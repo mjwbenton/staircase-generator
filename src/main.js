85202529d5c4460f7251c8779964c25a2d0f8087
Matthew Benton <me@mattbenton.co.uk>
2016-04-01 07:23:31 -0700

    Add basic react wrapper

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-
-import HtmlHeader from './components/HtmlHeader';
-
 import handleFrontMatter from './transforms/handle-front-matter';
 import handleMarkdown from './transforms/handle-markdown';
+import wrapReact from './transforms/wrap-react';
 import readContent from './read-content';
 import writeContent from './write-content';
 
 readContent('./content').catch((err) =>  console.log(err))
     .then(handleFrontMatter)
     .then(handleMarkdown)
+    .then(wrapReact)
     .then(writeContent('./output'))
     .then((result) => {
         console.log(result.items);
---
+++ src/transforms/wrap-react.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import HtmlHeader from '../components/HtmlHeader';
+import { skipMeta, skipDirectories } from './skip-items';
+
+export default function wrapReact(site) {
+    return site.mapWithFilters([skipMeta, skipDirectories], (item) => {
+        const DOCTYPE = '<!doctype html>';
+        const html = DOCTYPE + "\n" + ReactDOMServer.renderToStaticMarkup(
+            <html>
+                <HtmlHeader />
+                <body>
+                    <div dangerouslySetInnerHTML={{ __html: item.getContent() }} />
+                </body>
+            </html>
+        );
+        return item.withContent(html);
+    });
+}
