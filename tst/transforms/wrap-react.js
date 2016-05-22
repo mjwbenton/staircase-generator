c470a91098b1045cc328170082155253c46ed2a6
Matthew Benton <me@mattbenton.co.uk>
2016-05-22 13:56:18 -0700

    wrap-react test

---
+++ tst/transforms/wrap-react.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+import test from 'tape';
+import proxyquire from 'proxyquire';
+import Site from '../../src/site';
+import ContentItemBuilder from '../../src/content-item-builder';
+import React from 'react';
+
+const wrapReact = proxyquire('../../src/transforms/wrap-react', {
+    '../components/Page': {
+        default(props) {
+            return <div>{props.item.getContent()}</div>;
+        }
+    }
+}).default;
+
+test('wrapReact', (t) => {
+    const content = 'whatever';
+    const expectedContent = '<!doctype html>\n<div>whatever</div>';
+    const ci = new ContentItemBuilder(false, '/path/')
+        .withContent(content)
+        .build();
+    const site = new Site([ci]);
+    const newSite = wrapReact(site);
+    t.equals(newSite.items[0].getContent(), expectedContent);
+    t.end();
+});
