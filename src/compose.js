16d0dffb1a9ba4d97d43d011fcebb5b3fb9dacf5
Matthew Benton <me@mattbenton.co.uk>
2016-05-29 19:44:51 -0700

    Add generic error handling to compose function

--- src/compose.js
+++ src/compose.js
@@ -6,7 +6,12 @@ export default function compose(...funcs : Array<(site : Site) => Site>) {
     return (site : Site) => {
         let outputSite = site;
         funcs.forEach((func) => {
-            outputSite = func(outputSite);
+            try {
+                outputSite = func(outputSite);
+            } catch (err) {
+                console.error(`Error in transform function: ${func.name}`);
+                console.error(err);
+            }
         });
         return outputSite;
     };
