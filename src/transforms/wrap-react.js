e6295a07adfc4b09a1f03aac8f85da94af0fd143
Matthew Benton <m@mattb.tech>
2016-06-18 20:23:23 -0700

    Adding logging to wrap-react

--- src/transforms/wrap-react.js
+++ src/transforms/wrap-react.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import type Site from '../site';
+import { getLogger } from '../logging';
 
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
@@ -9,11 +10,16 @@ import { skipMeta, skipDirectories } from './skip-items';
 
 export default function wrapReactOuter(Component : any) {
     return function wrapReact(site : Site) {
-        return site.mapWithFilters([skipMeta, skipDirectories], (item) => {
-            const html = ReactDOMServer.renderToStaticMarkup(
-                <Component item={item} site={site} />
-            );
-            return item.withContent(html);
-        });
+        const log = getLogger('wrapReact');
+        const newSite = site.mapWithFilters([skipMeta, skipDirectories],
+            (item) => {
+                log.info(`Rendering page '${item.getFileName()}' with react`);
+                const html = ReactDOMServer.renderToStaticMarkup(
+                    <Component item={item} site={site} />
+                );
+                return item.withContent(html);
+            });
+        log.info('Finished wrapReact');
+        return newSite;
     };
 }
