ba412b832e28a52fb1a376f3f98e8d1f51f22540
Matthew Benton <m@mattb.tech>
2017-08-05 15:03:49 -0700

    Move clearFlickrCache to ts

--- clearFlickrCache.js
+++
@@ -1,5 +0,0 @@
-var Cache = require('async-disk-cache');
-var cache = new Cache('flickr-cache');
-cache.clear().then(function() {
-    console.log('Done')
-});
--- types/async-disk-cache.d.ts
+++ types/async-disk-cache.d.ts
@@ -3,5 +3,6 @@ declare module "async-disk-cache" {
         constructor(x: string);
         get(x: string): Promise<{ isCached: boolean, value: any }>;
         set(x: string, v: any): Promise<void>;
+        clear(): Promise<void>;
     }
 }
